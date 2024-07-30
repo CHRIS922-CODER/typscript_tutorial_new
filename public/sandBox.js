24f9754512cc36cc086acc1a4a7737631263e53d
CHRIS922-CODER <christopheromosa632@gmail.com>
2024-07-30 10:52:20 +0300

    funciton_basics

--- public/sandBox.js
+++ public/sandBox.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("testing 123");
+// let greet = ()=>{
+//     console.log('hello')
+// }
+// greet = 'hello'
+let greet;
+greet = () => {
+    console.log('hello again');
+};
+// const add = (a:number, b:number, c?:number | string )=>{
+//     return console.log(a + b);    
+// }
+const add = (a, b, c = 12) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 2, 12);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(23, 45);
+// result = ''
--- src/sandBox.ts
+++ src/sandBox.ts
@@ -1,1 +1,29 @@
-console.log("testing 123")
+// let greet = ()=>{
+//     console.log('hello')
+// }
+
+// greet = 'hello'
+let greet: Function;
+
+greet = () =>{
+    console.log('hello again')
+}
+
+// const add = (a:number, b:number, c?:number | string )=>{
+//     return console.log(a + b);    
+// }
+
+const add:Function = (a:number, b:number, c:number | string = 12)=>{    
+       
+    console.log(a + b);    
+    console.log(c);
+}
+add(5,2,12)
+
+
+const minus = (a:number, b:number)=>{
+return a + b;
+}
+let result = minus(23,45)
+
+// result = ''
