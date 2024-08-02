e08b4ed6d39c7edb5f2e75acd47175757f069d4d
CHRIS922-CODER <christopheromosa632@gmail.com>
2024-08-02 23:02:43 +0300

    interfaces

--- src/sandBox.ts
+++ src/sandBox.ts
@@ -1,33 +1,33 @@
-import {Invoice} from './classes/Invoices.js'
+import { Invoice } from './classes/Invoices.js'
 
 interface isPerson {
-name:string;
-age:number;
-speak(a:string):void;
-spend(a:number):number;
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
 }
 
 const me: isPerson = {
-    name:'shawn',
-    age:23,
-    speak(text:string):void {
+    name: 'shawn',
+    age: 23,
+    speak(text: string): void {
         console.log(text)
     },
-    spend(amount:number):number {
-        console.log('I spent ',amount)
+    spend(amount: number): number {
+        console.log('I spent ', amount)
         return amount;
-    }
+    },
+   
 
 }
 
 console.log(me)
-const greetPerson = (person: isPerson)=>{
-    console.log("Hello ",person.name)
+const greetPerson = (person: isPerson) => {
 }
 
 
-const invOne = new Invoice('chris','system update',456666)
-const invTwo = new Invoice('youngkim','Error and testing',45226)
+const invOne = new Invoice('chris', 'system update', 456666)
+const invTwo = new Invoice('youngkim', 'Error and testing', 45226)
 
 // console.log(invOne.format(),invTwo);
 
@@ -41,12 +41,11 @@ invoices.push(invTwo)
 console.log(invoices);
 // invOne.client = 'something else'
 
-invoices.forEach((inv)=>{
-    console.log(inv.client,inv.amount,inv.format());    
+invoices.forEach((inv) => {
+    console.log(inv.client, inv.amount, inv.format());
 })
 
 
 
 
-        
-    
+
