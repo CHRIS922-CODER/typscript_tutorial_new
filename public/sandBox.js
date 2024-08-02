446d7a5f1ab7501a61f96c1f09053841a5059e7d
CHRIS922-CODER <christopheromosa632@gmail.com>
2024-08-03 00:03:41 +0300

    interface with classes

--- public/sandBox.js
+++ public/sandBox.js
@@ -1,14 +1,39 @@
 import { Invoice } from './classes/Invoices.js';
-const invOne = new Invoice('chris', 'system update', 456666);
-const invTwo = new Invoice('youngkim', 'Error and testing', 45226);
-// console.log(invOne.format(),invTwo);
-// let invoices: string[]= [];
-let invoices = [];
-// invoices.push({name:"john"})
-invoices.push(invOne);
-invoices.push(invTwo);
-console.log(invoices);
-// invOne.client = 'something else'
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
+import { Payment } from './classes/payments.js';
+// let docOne : HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice('chirs','system approval',3455)
+// docTwo = new Payment('jane','data entry',456789)
+// let docs : HasFormatter[] =[]
+// docs.push(docOne)
+// docs.push(docTwo)
+// console.log(docs);
+// const invOne = new Invoice('chris', 'system update', 456666)
+// const invTwo = new Invoice('youngkim', 'Error and testing', 45226)
+// // console.log(invOne.format(),invTwo);
+// // let invoices: string[]= [];
+// let invoices: Invoice[] = []
+// // invoices.push({name:"john"})
+// invoices.push(invOne)
+// invoices.push(invTwo)
+// console.log(invoices);
+// // invOne.client = 'something else'
+// invoices.forEach((inv) => {
+//     console.log(inv.client, inv.amount, inv.format());
+// })
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/sandBox.ts
+++ src/sandBox.ts
@@ -1,51 +1,63 @@
 import { Invoice } from './classes/Invoices.js'
+import { Payment } from './classes/payments.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
-interface isPerson {
-    name: string;
-    age: number;
-    speak(a: string): void;
-    spend(a: number): number;
+// let docOne : HasFormatter;
+// let docTwo: HasFormatter;
+
+// docOne = new Invoice('chirs','system approval',3455)
+// docTwo = new Payment('jane','data entry',456789)
+
+
+// let docs : HasFormatter[] =[]
+
+// docs.push(docOne)
+// docs.push(docTwo)
+
+// console.log(docs);
+
+// const invOne = new Invoice('chris', 'system update', 456666)
+// const invTwo = new Invoice('youngkim', 'Error and testing', 45226)
+
+// // console.log(invOne.format(),invTwo);
+
+// // let invoices: string[]= [];
+
+// let invoices: Invoice[] = []
+// // invoices.push({name:"john"})
+// invoices.push(invOne)
+// invoices.push(invTwo)
+
+// console.log(invoices);
+// // invOne.client = 'something else'
+
+// invoices.forEach((inv) => {
+//     console.log(inv.client, inv.amount, inv.format());
+// })
+
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+const type = document.querySelector('#type')as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const  details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+
+form.addEventListener('submit', (e:Event)=>{
+    e.preventDefault();
+let doc :HasFormatter ;
+if(type.value === 'invoice'){
+doc = new Invoice(tofrom.value,details.value,amount.valueAsNumber)
+}else{
+doc = new Payment(tofrom.value, details.value,amount.valueAsNumber)
 }
 
-const me: isPerson = {
-    name: 'shawn',
-    age: 23,
-    speak(text: string): void {
-        console.log(text)
-    },
-    spend(amount: number): number {
-        console.log('I spent ', amount)
-        return amount;
-    },
-   
 
-}
-
-console.log(me)
-const greetPerson = (person: isPerson) => {
-}
-
-
-const invOne = new Invoice('chris', 'system update', 456666)
-const invTwo = new Invoice('youngkim', 'Error and testing', 45226)
-
-// console.log(invOne.format(),invTwo);
-
-// let invoices: string[]= [];
-
-let invoices: Invoice[] = []
-// invoices.push({name:"john"})
-invoices.push(invOne)
-invoices.push(invTwo)
-
-console.log(invoices);
-// invOne.client = 'something else'
-
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
+    console.log(
+        doc
+    );
+    
 })
-
-
-
-
 
